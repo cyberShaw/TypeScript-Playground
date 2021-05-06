d783301a95d0f08eb733cce5bd4283e9e13fe540
Saiharsha B <saiharsha.b@flipkart.com>
2021-05-06 10:23:54 +0530

    Add POST route

--- TypeScript API/src/apiRoutes.ts
+++ TypeScript API/src/apiRoutes.ts
@@ -1,5 +1,8 @@
 import { RoutesConfig } from "./routes.config";
-import express from "express";
+import express, { request } from "express";
+import fetch from "node-fetch";
+
+let data: Object[] = [];
 
 export class ApiRoutes extends RoutesConfig {
   constructor(app: express.Application) {
@@ -19,6 +22,19 @@ export class ApiRoutes extends RoutesConfig {
           `GET Request successful. Parameter passed is: ${req.params.id}`
         );
       });
+    this.app
+      .route("/api/post")
+      .post((req: express.Request, res: express.Response) => {
+        let putData = {
+          id: req.body.id,
+          time: new Date(),
+        };
+        data.push(putData);
+        console.log(data);
+        res.json(putData);
+      });
     return this.app;
   }
 }
+
+// curl --request POST -H "Content-Type: application/json" -d '{"id": "5"}' localhost:3000/api/post
